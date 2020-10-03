8f1c49b3bcbb1c8f755d17f244c27a47d22ab4ba
talencote <vitkomax01@gmail.com>
2020-10-03 20:16:06 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let min = array[0];
+  array.forEach(element => {
+    if (element < min) {
+      if (element === NaN || element === undefined) {
+        return 0;
+      }
+      min = element;
+    }
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let max = array[0];
+  array.forEach(element => {
+    if (element > max) {
+      if (element === NaN || element === undefined) {
+        return 0;
+      }
+      max = element;
+    }
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    if (element === NaN || element === undefined) {
+      return 0;
+    }
+    sum += element;
+  })
+  return sum / array.length; 
 }
